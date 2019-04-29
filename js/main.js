04e54fdcf5b6fe297a61161c2ad3d6028ba503a3
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-04-29 12:17:58 -0300

    mudancas iniciais
    add css, js folders;
    add index.html page simple.
    learning more about

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var msg = "Hello JavaScript";
+console.log(msg);
+
+var informaNomeDiv = document.getElementById("informaNome");//Manipulação via DOM
+informaNomeDiv.innerHTML = "<p>This is from JavaScript</p>";
+
